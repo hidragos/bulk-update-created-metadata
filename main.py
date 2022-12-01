bf2962d06ddf5d16c451c74d256c64ab32acd8b6
Dragos Iliescu <dragos@allroundzonwering.nl>
2022-12-01 22:18:05 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,48 @@
+# !/bin/python3
+# Python3 script to add one year to the Created date of the argument file
+# Usage: script.py <file>
+
+import os
+import sys
+import time
+from subprocess import call
+
+
+def add_year(file):
+    '''
+    Adds one year to the Created date of the argument file
+    '''
+    # Get the file's creation date
+    stat = os.stat(file)
+    created = stat.st_birthtime
+
+    # Add one year to the creation date
+    created += 31536000
+
+    # Format the date to MM/DD/YYYY
+    date = time.strftime('%m/%d/%Y', time.localtime(created))
+    t = time.strftime('%H:%M:%S', time.localtime(created))
+
+    # Example: command = 'SetFile -d ' + '"05/06/2019 ' + '22:44:33"' + file
+    # Log date
+    command = 'SetFile -d ' + '"' + date + ' ' + t + '"' + ' ' + file
+    call(command, shell=True)
+
+
+def add_year_to_all_files():
+    '''
+    Adds one year to the Created date of all files in the argument directory
+    '''
+    # Get the directory
+    directory = sys.argv[1]
+
+    # Get all files in the directory
+    files = os.listdir(directory)
+
+    # Add one year to the Created date of each file
+    for file in files:
+        add_year(file)
+
+
+if __name__ == '__main__':
+    add_year_to_all_files()
---
+++ readme.md
@@ -0,0 +1,23 @@
+## 📆 Bulk update Created metadata of files
+
+### 💡 Description
+
+This script will add a year to the _Created_ metadata of all files in a folder.
+Useful for when you have a bunch of files that were created in the wrong year.
+
+- Limited use cases, but it was exactly what I needed.
+- Works on MacOS Ventura 13.0 on Apple Silicon (M1) processor.
+
+### 📝 Usage
+
+```
+python3 main.py <path>
+```
+
+### 🤔 Roadmap
+
+- [ ] Add support for filters
+  - [ ] File extension
+  - [ ] File name
+  - [ ] File size
+- [ ] Add support for custom date input
